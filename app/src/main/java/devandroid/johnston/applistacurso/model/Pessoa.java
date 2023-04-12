f2cc681c988b53c9f7e9cf9e517f40a9414ba05b
humbertojohnston <hjpinheirojr@gmail.com>
2023-04-12 20:47:10 -0300

    Criação Classe Pessoa MODEL

---
+++ app/src/main/java/devandroid/johnston/applistacurso/model/Pessoa.java
@@ -0,0 +1,10 @@
+package devandroid.johnston.applistacurso.model;
+
+public class Pessoa {
+
+    //construtor
+    public Pessoa() {
+
+
+    }
+}
--- app/src/main/java/devandroid/johnston/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/johnston/applistacurso/view/MainActivity.java
@@ -1,16 +1,21 @@
 package devandroid.johnston.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 import devandroid.johnston.applistacurso.R;
+import devandroid.johnston.applistacurso.model.Pessoa;
+
 
 public class MainActivity extends AppCompatActivity {
+
+ //   Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Pessoa pessoa = new Pessoa();
+
     }
 }
