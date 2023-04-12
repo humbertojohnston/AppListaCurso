41d509c9463c3f77b4781a74b418f48265ab8819
humbertojohnston <hjpinheirojr@gmail.com>
2023-04-12 09:15:45 -0300

    Arquitetura MVC

--- app/src/main/java/devandroid/johnston/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/johnston/applistacurso/view/MainActivity.java
@@ -1,8 +1,10 @@
-package devandroid.johnston.applistacurso;
+package devandroid.johnston.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import devandroid.johnston.applistacurso.R;
 
 public class MainActivity extends AppCompatActivity {
 
