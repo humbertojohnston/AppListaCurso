fac8363affc5719e81f6590b3a8ba0dfc2931055
humbertojohnston <hjpinheirojr@gmail.com>
2023-04-13 09:05:42 -0300

    Uso dos métodos GET e SET.

--- app/src/main/java/devandroid/johnston/applistacurso/model/Pessoa.java
+++ app/src/main/java/devandroid/johnston/applistacurso/model/Pessoa.java
@@ -2,8 +2,48 @@ package devandroid.johnston.applistacurso.model;
 
 public class Pessoa {
 
-    //construtor
-    public Pessoa() {
+    //Método Construtor
+    //Atributos - Objetos - Moldes - Templates
+    //Métodos de Acesso - Set's e Get's
+
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    public Pessoa() { //método Pessoa
 
 
     }
--- app/src/main/java/devandroid/johnston/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/johnston/applistacurso/view/MainActivity.java
@@ -9,6 +9,8 @@ import devandroid.johnston.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
  //   Pessoa pessoa;
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Pessoa pessoa = new Pessoa();
+        //Atribuir conteúdo, dados, valores para o objeto
+        //Conformw o seu MODELO, TEMPLATE
+        pessoa.setPrimeiroNome("Humberto");
+        pessoa.setSobreNome("Pinheiro");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("91 981895467");
+
+        Pessoa outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Alicia");
+        outraPessoa.setSobreNome("Pinheiro");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("91 981891234");
+
+       dadosPessoa = "Primeiro nome: ";
+       dadosPessoa += pessoa.getPrimeiroNome();
+       dadosPessoa += " Sobrenome ";
+       dadosPessoa += pessoa.getSobreNome();
+       dadosPessoa += " Curso desejado: ";
+       dadosPessoa += pessoa.getCursoDesejado();
+       dadosPessoa += " Telefone de contato ";
+       dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de contato ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+
+
+
 
     }
 }
